a6e61d37f51ffd4ba082f7f621f8a54ca49dd624
aguustinagodoy <aguustina.ge@hotmail.com>
2024-08-20 21:53:02 -0300

    ejercicio clase

---
+++ script.js
@@ -0,0 +1,25 @@
+
+window.onload=function(){
+
+    
+    var ventana = document.getElementById ("carga");
+    setTimeout(() => {
+        ventana.style.display="none";
+
+
+    },1000);
+
+var bienvenida= document.getElementById ("bienvenida")
+setTimeout(() => {
+    bienvenida.style.display="block";
+
+},5000);
+
+
+
+}
+
+
+
+
+   
